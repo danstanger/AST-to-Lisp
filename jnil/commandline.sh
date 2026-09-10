20c5177e3d7d5314b4a44bca32ea23ba8d3dbe8a
danstanger <dan.stanger@ieee.org>
2026-09-10 14:29:44 -0400

    Add command line to run dbgprint

--- jnil/commandline.sh
+++ jnil/commandline.sh
@@ -1,1 +1,3 @@
 clisp -q -repl -i asdf -x "(asdf:operate 'asdf:load-source-op \"jnil\")" -x "(javatools.jlinker::read-java-ast nil nil)" -x "(setf *x* (apply #'jnil.ast::process nil javatools.jlinker::*java-ast*))" -x "(setf *c* (jnil::jnil-generate-code *x* :common-lisp t nil))"
+
+clisp -q -repl -i asdf -x "(asdf:operate 'asdf:load-source-op \"jnil\")" -x "(javatools.jlinker::read-java-ast nil nil)" -x "(setf *x* (apply #'jnil.ast::process nil javatools.jlinker::*java-ast*))" -x "(load \"dbgprint\")" -x "(dbgprint *x*)"
